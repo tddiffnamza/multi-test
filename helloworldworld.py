63b29c9f1ac7f4861d3f1ea09976c2f5a3624cdd
Unknown <60070136@kmitl.ac.th>
2017-10-05 10:48:46 +0700

    เพิ่ม Fn ลบ
    n1 - n2

--- helloworldworld.py
+++ helloworldworld.py
@@ -1,6 +1,9 @@
 def cal(num1, num2):
-    summ = sum(num1, num2)
-    subb = sub(num1, num2)
+    summ = summation(num1, num2)
+    subb = subtraction(num1, num2)
     return subb, summ
+
+def subtraction(n1, n2)
+    return n1 - n2
 
 cal(int(input()), int(input()))
