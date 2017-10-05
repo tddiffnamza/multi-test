2c01c83f0fcd18e9ce9dbe5b3ad575f8aacce285
Unknown <60070133@kmitl.ac.th>
2017-10-05 10:48:11 +0700

    เพิ่ม Fn บวก
    n1 + n2

--- helloworldworld.py
+++ helloworldworld.py
@@ -1,6 +1,10 @@
 def cal(num1, num2):
-    summ = sum(num1, num2)
-    subb = sub(num1, num2)
+    summ = summation(num1, num2)
+    subb = subtraction(num1, num2)
     return subb, summ
 
+def summation(n1, n2):
+    return n1 + n2
+
 cal(int(input()), int(input()))
+def sum
